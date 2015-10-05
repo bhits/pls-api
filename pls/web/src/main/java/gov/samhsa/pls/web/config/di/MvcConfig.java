3c6d4023f8c5f40914b5b710ddc23b5f3d2d3412
Feruz <feruz.abdella@feisystems.com>
2015-10-05 13:08:57 -0400

    Add CORS support to pls api
    #7059 - Create API for Provider Lookup

--- pls/web/src/main/java/gov/samhsa/pls/web/config/di/MvcConfig.java
+++ pls/web/src/main/java/gov/samhsa/pls/web/config/di/MvcConfig.java
@@ -6,10 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
@@ -21,6 +18,11 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackageClasses = {ControllerBasePackageMarker.class})
 public class MvcConfig extends WebMvcConfigurerAdapter {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
 
 	@Override
 	public void configureDefaultServletHandling(
