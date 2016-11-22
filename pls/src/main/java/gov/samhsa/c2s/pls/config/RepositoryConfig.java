5eebd2d3c225045e65a5d6e88fa5762e3c65281f
CindyLiFei <cindy.li@feisystems.com>
2016-11-22 13:03:50 -0500

    Expose code in EntityType domain.
    # 11657 - As a developer, I will have upgraded PLS to Spring Boot Application, so that the PLS code is more maintainable

--- pls/src/main/java/gov/samhsa/c2s/pls/config/RepositoryConfig.java
+++ pls/src/main/java/gov/samhsa/c2s/pls/config/RepositoryConfig.java
@@ -1,6 +1,7 @@
 package gov.samhsa.c2s.pls.config;
 
 import gov.samhsa.c2s.pls.domain.provider.Provider;
+import gov.samhsa.c2s.pls.domain.reference.EntityType;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
@@ -11,5 +12,6 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Provider.class);
+        config.exposeIdsFor(EntityType.class);
     }
 }
